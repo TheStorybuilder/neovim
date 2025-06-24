8536c0fab6bd79182135c1ad675991d1033dc1f6
Raul Velazquez <raul@storybuildere.com>
2025-06-23 21:40:48 -0700

    lazyvim_062325

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.winbar = "%=%m %f %h%w%r%{&ff} %y %l:%c %p%%"
+vim.g.lazyvim_eslint_auto_format = true
