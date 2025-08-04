b5fe790364b9a022d4f520fd609200b3d6ae677b
Raul Velazquez <raul@storybuildere.com>
2025-08-03 23:06:58 -0700

    lazyvim_080325

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,12 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      elixir = { "mix" },
+      eelixir = { "mix" },
+      heex = { "mix" },
+      surface = { "mix" },
+      -- Add other file types as needed
+    },
+  },
+}
